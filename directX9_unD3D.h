1bbec8ec8b1abbc0c379dd9a7a0f0dd05c7a2e47
fs820 <241308@st.yoshida-g.ac.jp>
2025-04-27 20:34:04 +0900

    a

---
+++ directX9_unD3D.h
@@ -0,0 +1,32 @@
+//-----------------------------------------------------
+//  
+// DirectX9èàóù(D3DXîpé~î≈)ÇÃêÈåæ [directX9_unD3D.h]  
+// Author: Fuma Sato  
+//  
+//-----------------------------------------------------
+#pragma once
+
+#include "d3d9.h"
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXCollision.h>
+#include <DirectXColors.h>
+#include <DirectXPackedVector.h>
+#include <DirectXTex.h>
+#include <SpriteBatch.h>
+#include <SpriteFont.h>
+#include <GeometricPrimitive.h>
+#include <Model.h>
+#include <Effects.h>
+#include <PrimitiveBatch.h>
+#include <CommonStates.h>
+#include <Keyboard.h>
+#include <Mouse.h>
+#include <GamePad.h>
+#include <SimpleMath.h>
+#include <WICTextureLoader.h>
+#include <DDSTextureLoader.h>
+#include <ScreenGrab.h>
+
+#pragma comment(lib,"DirectXTK.lib")
+#pragma comment(lib,"DirectXTex.lib")
--- main.cpp
+++ main.cpp
@@ -4,3 +4,9 @@
 // Author: Fuma Sato
 //
 //-----------------------------------
+#include "main.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstancePrev, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	return 0;
+}
