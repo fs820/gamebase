09d8cdb989ea041fd6448366edf389f1cd845411
fs820 <241308@st.yoshida-g.ac.jp>
2025-04-28 12:58:57 +0900

    そ

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 //
 //-----------------------------------
 #include "main.h"
+#include <windows.h>
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hInstancePrev, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
